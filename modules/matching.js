e2010e7f904d07b8deada99e7b6e3058a4a89e37
pcs <mcfly2024@gmail.com>
2014-08-22 02:14:40 -0700

    pcs

--- modules/matching.js
+++ modules/matching.js
@@ -50,6 +50,7 @@ function matching(gps_x, gps_y, time_info,facebook_info,req,res){
 			j=j+60;
 
 		}
+		console.log(almost_same[j])
 		if(almost_same[j]!=undefined&&almost_same[j].length>0)
 		{
 			for(var inner in almost_same){
